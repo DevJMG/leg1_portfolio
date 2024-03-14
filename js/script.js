39e73de26cc5d831ded11fb267448ceed024e26e
John Michael Gadot <jmgadot.dev@gmail.com>
2024-03-14 13:05:32 +0800

    Initial source code of the project.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// TOGGLE BUTTON
+	const toggleMenu = document.querySelector("#toggle_menu");
+	const menu = document.querySelector(".menu");
+
+	toggleMenu.addEventListener("click", function() {
+		menu.classList.toggle("show");
+	});
+// 
